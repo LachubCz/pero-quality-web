83d70a256535191eddf694d0c34c686d389daf42
LachubCz <petr.genius.buchal@gmail.com>
2019-08-13 11:33:57 +0200

    Ordering fixed.

--- app/__init__.py
+++ app/__init__.py
@@ -148,8 +148,8 @@ def create_app():
 
         return render_template("comparing.html", record_id=rnd_record.id, record_crops=enumerate(record_crops))
 
-    @app.route('/ordering/<set>', methods = ['GET', 'POST'])
-    def show_ordering(set):
+    @app.route('/ordering/<set_id>', methods = ['GET', 'POST'])
+    def show_ordering(set_id):
         user = user_cookie()
         if request.method == 'POST':
             print(request.form)
@@ -157,33 +157,16 @@ def create_app():
                          int(request.form['milisec'])*10000)
             add_annotation(user.id, request.form['record'], request.form['order'], time_)
 
-        set_ = Set.query.filter_by(id=set).first()
+        set_ = Set.query.get(set_id)
         if set_.type != 1 or set_.active == False:
             abort(404)
 
-        rnd_record_id = Record.query.filter_by(set_id=set).order_by(func.random()).first().id
-        statement = db_session.query(RecordCrop).filter_by(record_id=rnd_record_id)
-        record_crop_ = db_session.execute(statement).fetchall()
-        crops_ = []
+        rnd_record = db_session.query(Record).filter(Record.set_id==set_id).order_by(Record.position.desc()).first()
+        rnd_record.position -= 10000
+        db_session.commit()
+        record_crops = db_session.query(RecordCrop).filter(RecordCrop.record_id==rnd_record.id).order_by(RecordCrop.order).all()
 
-        record_crop_.sort(key=lambda tup: tup[2])
-        for i, item in enumerate(record_crop_):
-            crops_.append(Crop.query.filter(Crop.id==record_crop_[i][1]).all())
-
-        full_filenames = []
-        for i, crop_ in enumerate(crops_):
-            if crop_[0].cropped:
-                full_filenames.append((i, str(crop_[0].id)+'.jpg'))
-            else:
-                page_ = Page.query.filter(Page.name==crop_[0].page_id).all()
-                img = cv2.imread(os.path.join(page_[0].path, crop_[0].page_id+'.jpg'))
-                crop_img = img[crop_[0].y:crop_[0].y+crop_[0].height, crop_[0].x:crop_[0].x+crop_[0].width]
-                cv2.imwrite(os.path.join(app.config['CROPS_PATH'], str(crop_[0].id)+'.jpg'), crop_img)
-                full_filenames.append((i, str(crop_[0].id)+'.jpg'))
-                crop_[0].cropped = True
-                db_session.commit()
-
-        return render_template("ordering.html", record_id = rnd_record_id, images_for_annotation = full_filenames)
+        return render_template("ordering.html", record_id=rnd_record.id, record_crops=enumerate(record_crops))
 
     @app.route('/rating/<set>', methods = ['GET', 'POST'])
     def show_rating(set):
